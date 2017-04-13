81b9c7657b7e29224c07e953c059f077f5504c70
Cheah Hong Loo <Cheah Hong Loo>
2017-04-13 15:47:48 +0800

    'a'

--- Food-Chain Crypto/src/FoodChainCrypto/LocalCryptoServer.java
+++ Food-Chain Crypto/src/FoodChainCrypto/LocalCryptoServer.java
@@ -56,9 +56,9 @@ public class LocalCryptoServer {
     private static final String alias = "tomcat";
     private static final char[] keyPass = "changeit".toCharArray();
     
-    private static final String keystoreFilePath = "Kch\\.keystore";
-    private static final String certFilePath = "Kch\\Kch.pem";
-    private static final String locationFilePath = "Kch\\Kch.txt";
+    private static final String keystoreFilePath = "Mri\\fake.keystore";
+    private static final String certFilePath = "Mri\\Mri.pem";
+    private static final String locationFilePath = "Mri\\Mri.txt";
     
     private static PrivateKey privKey;
     private static KeyStore keyStore;
